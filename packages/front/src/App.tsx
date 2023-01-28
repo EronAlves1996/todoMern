a26fe1f4c0388e36c99316a32021eea86e253f42
Eron Alves <eron.a.silva@capgemini.com>
2023-01-28 11:06:43 -0300

    feat: start to create Login page

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <form action="">
+        <label htmlFor="">E-mail</label>
+        <input type="text" name="email" id="email" />
+        <label htmlFor="">Senha</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit">Login</button>
+      </form>
+      <a href="">Esqueceu sua senha?</a>
+      <a href="">Criar nova conta</a>
+    </>
+  );
 }
 
-export default App
+export default App;

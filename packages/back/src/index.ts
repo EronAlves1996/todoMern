f65cebef196a84c7ac11db4f6b0c023fe1846c83
Eron Alves <eron.a.silva@capgemini.com>
2023-02-01 01:03:44 -0300

    feat: expose verifyMiddleware

--- packages/back/src/index.ts
+++ packages/back/src/index.ts
@@ -2,11 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import graphqlMiddleware from "./graphqlMiddleware";
 import loginMiddleware from "./loginMiddleware";
+import verifyMiddlware from "./verifyMiddleware";
 
 const app = express();
 
 app.post("/login", loginMiddleware);
-
+app.get("/verify", verifyMiddlware);
 app.all("/graphql", graphqlMiddleware);
 
 (async () => {

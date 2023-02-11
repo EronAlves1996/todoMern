a478ce232652e8cc2208cc2a150e6dd3451eb50a
Eron Alves <eron.a.silva@capgemini.com>
2023-02-11 10:16:31 -0300

    test: make act for test

--- packages/front/src/pages/Index/children/Registrar/RegisterForm.test.tsx
+++ packages/front/src/pages/Index/children/Registrar/RegisterForm.test.tsx
@@ -1,11 +1,11 @@
 import { describe, it } from "@jest/globals";
 import { act } from "react-dom/test-utils";
 import { FieldValues, SubmitHandler } from "react-hook-form";
-import { render, screen } from "../../../../utils/test-utils";
+import { fireEvent, render, screen } from "../../../../utils/test-utils";
 import { RegisterForm } from "./RegisterForm";
 
 describe("register form", () => {
-  it("should register and send data", () => {
+  it("should register and send data", async () => {
     let dataSubmitted;
 
     const submitterMock: SubmitHandler<FieldValues> = (data) => {
@@ -14,10 +14,26 @@ describe("register form", () => {
 
     render(<RegisterForm submitter={submitterMock} />);
 
-    const nameForm = screen.findByLabelText("Nome");
-    const emailForm = screen.findByLabelText("E-mail");
-    const passwordForm = screen.findByLabelText("Senha");
-    const repeatPasswordForm = screen.findByLabelText("Repita sua Senha");
-    const form = screen.findByRole("form");
+    const nameForm = await screen.findByLabelText("Nome");
+    const emailForm = await screen.findByLabelText("E-mail");
+    const passwordForm = await screen.findByLabelText("Senha");
+    const repeatPasswordForm = await screen.findByLabelText("Repita sua Senha");
+    const form = await screen.findByRole("form");
+
+    await act(async () => {
+      await typeIn(nameForm, "Test user");
+      await typeIn(emailForm, "test@test");
+      await typeIn(passwordForm, "1234");
+      await typeIn(repeatPasswordForm, "1234");
+      await fireEvent.submit(form);
+    });
   });
 });
+
+async function typeIn(element: HTMLElement, value: string) {
+  await fireEvent.change(element, {
+    target: {
+      value,
+    },
+  });
+}

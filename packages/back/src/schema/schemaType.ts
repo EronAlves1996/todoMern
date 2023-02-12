2b069a1fc67a47849e865f2d107f1c95a56b81d9
Eron Alves <eron.a.silva@capgemini.com>
2023-02-12 13:47:38 -0300

    refactor: extract mongooseSchemaDef type

--- packages/back/src/schema/schemaType.ts
+++ packages/back/src/schema/schemaType.ts
@@ -1,16 +1,13 @@
-import {
-  GraphQLFieldConfig,
-  GraphQLInputType,
-  GraphQLNamedType,
-  GraphQLOutputType,
-} from "graphql";
+import { GraphQLFieldConfig, GraphQLNamedType } from "graphql";
 import { Schema } from "mongoose";
+
+export type mongooseSchemaDef = { name: string; schema: Schema };
 
 interface Ischema {
   types?: GraphQLNamedType[];
   queries?: Array<GraphQLFieldConfig<any, any, any>>;
   mutations?: Array<GraphQLFieldConfig<any, any, any>>;
-  mongooseSchema?: { name: string; schema: Schema };
+  mongooseSchema?: mongooseSchemaDef;
 }
 
 export default Ischema;

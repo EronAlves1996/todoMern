bddab8a322acb44b8fac836b42884cfdff43126c
Eron Alves <eron.a.silva@capgemini.com>
2023-02-12 14:10:20 -0300

    refactor: conform middlewares to the new structure

--- packages/back/src/middlewares/verifyMiddleware.ts
+++ packages/back/src/middlewares/verifyMiddleware.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import configuration from "../config";
-import userDbAccess from "../dbAccess/user";
 import { erasePasswordInformation } from "../utils/userUtils";
+import { loaders } from "./graphqlMiddleware";
 import { send403 } from "./utils";
 
-const { userExistsById, findById } = userDbAccess;
+const { existsBy, findOneBy } = loaders.user;
 
 function invalidateCookie(res: Response) {
   res.cookie(configuration.COOKIE_NAME, "", { maxAge: 0 });
@@ -19,10 +19,10 @@ async function verifyMiddlware(
   try {
     const cookie = req.cookies[configuration.COOKIE_NAME];
     const userId = verify(cookie, configuration.JWT_SECRET) as string;
-    if (!userExistsById(userId)) {
+    if (!existsBy({ _id: userId })) {
       throw new Error("Invalid cookie/user");
     }
-    const user = await findById(userId);
+    const user = await findOneBy({ _id: userId });
     const userWithoutPassword = erasePasswordInformation(user?.toObject());
     res.status(200);
     res.send(userWithoutPassword);

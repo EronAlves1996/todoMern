70dd8a4258863dbcd906bea88358805554ae2915
Eron Alves <eron.a.silva@capgemini.com>
2023-02-20 00:18:31 -0300

    feat: add query to load task data into form

--- packages/front/src/pages/Index/children/Home/TaskDisplay.tsx
+++ packages/front/src/pages/Index/children/Home/TaskDisplay.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import {
+  graphql,
   GraphQLTaggedNode,
+  loadQuery,
   PreloadedQuery,
   usePreloadedQuery,
 } from "react-relay";
-import { loadTasks } from ".";
+import { RelayEnvironment } from "../../../../RelayEnvironment";
+import EditTaskForm from "./EditTaskForm";
 import { HomeQuery } from "./__generated__/HomeQuery.graphql";
+import { TaskDisplayQuery } from "./__generated__/TaskDisplayQuery.graphql";
+
+const taskDisplayQuery = graphql`
+  query TaskDisplayQuery($taskId: String!) {
+    loadTask(id: $taskId) {
+      _id
+      description
+      deadline
+      isCompleted
+      creationDate
+    }
+  }
+`;
 
 function TaskRow({
   task,
@@ -16,11 +33,33 @@ function TaskRow({
     readonly isCompleted: boolean;
   } | null;
 }) {
+  const [showTaskForm, setShowTaskForm] = useState(false);
+  const [query, setQuery] = useState<PreloadedQuery<TaskDisplayQuery> | null>(
+    null
+  );
+
   return (
     <div>
       <p>{task?.description}</p>
       <p>{task?.deadline}</p>
       <p>{task?.isCompleted.toString()}</p>
+      <button
+        onClick={() => setShowTaskForm(!showTaskForm)}
+        onMouseEnter={() => {
+          setQuery(
+            loadQuery(RelayEnvironment, taskDisplayQuery, {
+              taskId: task?._id!,
+            })
+          );
+        }}
+      >
+        {showTaskForm ? <span>Cancelar</span> : <span>Editar</span>}
+      </button>
+      <EditTaskForm
+        query={query!}
+        graphqlTag={taskDisplayQuery}
+        showControllers={[showTaskForm, setShowTaskForm]}
+      />
     </div>
   );
 }
@@ -36,7 +75,7 @@ export function TaskDisplay({
   return (
     <>
       {data.loadTasks!.map((t) => (
-        <TaskRow task={t} />
+        <TaskRow task={t} key={t?._id} />
       ))}
       ;
     </>

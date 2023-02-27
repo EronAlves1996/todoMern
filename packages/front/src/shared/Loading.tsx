0f2a8514fedf5e3f1a87abb2486feb01a2900c9e
Eron Alves <eron.a.silva@capgemini.com>
2023-02-27 01:03:07 -0300

    fix: Loading position

--- packages/front/src/shared/Loading.tsx
+++ packages/front/src/shared/Loading.tsx
@@ -2,17 +2,19 @@ import { Oval } from "react-loader-spinner";
 
 export default function Loading() {
   return (
-    <Oval
-      height={80}
-      width={80}
-      color="#4fa94d"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-      ariaLabel="oval-loading"
-      secondaryColor="#4fa94d"
-      strokeWidth={3}
-      strokeWidthSecondary={3}
-    />
+    <div className="self-center">
+      <Oval
+        height={80}
+        width={80}
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="#4fa94d"
+        strokeWidth={3}
+        strokeWidthSecondary={3}
+      />
+    </div>
   );
 }
